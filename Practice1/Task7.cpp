c3f802ade71d1cdc36cf75543b8cc773d3fb112e
Дарья <104324711+Dariar-Danire@users.noreply.github.com>
2023-10-01 15:50:15 +0300

    Create Task7.cpp

---
+++ Practice1/Task7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int a, b; 
+    cin >> a >> b;
+    
+    if(a > b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
+}
